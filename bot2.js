e762125a006cd7ba65000cf10124135b2699b272
jouvett <jouvett.garcia@gmail.com>
2017-05-27 23:20:05 +0200

    bl

--- bot2.js
+++ bot2.js
@@ -70,7 +70,8 @@ bot.on('message', message => {   //A chaque fois qu'un message est envoye; on va
 		axios.get('https://www.chucknorrisfacts.fr/api/get?data=tri:alea;nb:1')
 			.then(function(response){
 				//message.channel.sendMessage(JSON.stringify(response.data,null,4));
-				message.channel.sendMessage(JSON.parse(response).fact);
+
+				message.channel.sendMessage(response.data.fact);
 				//console.log(response.data);
 				//console.log(response.status);
 			});
