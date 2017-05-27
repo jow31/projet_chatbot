640b5e5013f552ba46daea36f198e792a5a35b10
jouvett <jouvett.garcia@gmail.com>
2017-05-27 17:34:39 +0200

    js

---
+++ blague.js
@@ -0,0 +1,7 @@
+module.exports = function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;      
+
+};
