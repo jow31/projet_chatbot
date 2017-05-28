c9e8221903c4e193024541d15a7f52745df0427c
jouvett <jouvett.garcia@gmail.com>
2017-05-28 10:06:59 +0200

    bl

--- bot2.js
+++ bot2.js
@@ -73,7 +73,7 @@ bot.on('message', message => {   //A chaque fois qu'un message est envoye; on va
 				//self.fact = response.data.fact;
 				//var resp = parser.parse(response.data);
 				//message.channel.sendMessage(JSON.stringify(response.data, null, 4)); //FUNCIONA
-				message.reply(response.data);
+				message.reply(response.data.toString());
 				//console.log(JSON.stringify(response.data, null, 4));
 				//resp = JSON.parse(response.data);
 				//message.channel.sendMessage(resp.fact); 
